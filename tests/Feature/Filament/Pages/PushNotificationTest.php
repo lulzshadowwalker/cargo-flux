a76d9eb378c223d2db87f9475ab30faaa24c78f7
lulzshadowwalker <lulzshadowwalker@gmail.com>
2024-12-12 23:27:44 +0300

    Added smoke test to the push notifications page

--- tests/Feature/Filament/Pages/PushNotificationTest.php
+++ tests/Feature/Filament/Pages/PushNotificationTest.php
@@ -4,11 +4,21 @@ namespace Tests\Feature\Filament\Pages;
 
 use App\Enums\Audience;
 use App\Filament\Pages\PushNotification;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Livewire\Livewire;
 use Tests\TestCase;
+use Tests\Traits\WithAdmin;
 
 class PushNotificationTest extends TestCase
 {
+    use RefreshDatabase, WithAdmin;
+
+    public function test_it_renders_the_page(): void
+    {
+        $this->get(route('filament.dashboard.pages.push-notification'))
+            ->assertOk();
+    }
+
     public function test_it_sends_a_notification()
     {
         Livewire::test(PushNotification::class)
