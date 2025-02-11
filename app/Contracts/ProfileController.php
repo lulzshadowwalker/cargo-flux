30c51a1b02c9a5cc94913edbbc60a3bba5032dad
lulzshadowwalker <lulzshadowwalker@gmail.com>
2025-02-11 15:43:23 +0300

    Removed TestGeoapify.php

--- app/Contracts/ProfileController.php
+++ app/Contracts/ProfileController.php
@@ -2,6 +2,8 @@
 
 namespace App\Contracts;
 
+//  NOTE: Not sure if I would rather have a controller interface or some service
+//  could use a service that accepts a params object not necessarily a request object. but validation ? hmm.
 interface ProfileController
 {
     public function index();
